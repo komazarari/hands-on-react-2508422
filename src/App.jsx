d6c4d711cce170cd0bcf1d7eec5fe35cb3181f07
Takuto Komazaki <1654804+komazarari@users.noreply.github.com>
2022-11-25 02:06:26 +0000

    fetch component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component } from 'react';
 import Welcome from './components/Welcome';
 import Support from './components/Support';
+import ListCast from './components/ListCast';
 
 function App() {
   const name = 'StarGazers'
@@ -15,6 +16,7 @@ function App() {
           <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <ListCast />
           <Support />
         </hgroup>
       </article>
