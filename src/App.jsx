a2d37f1929b701348d74e8141740e20256bfc1f0
Takuto Komazaki <1654804+komazarari@users.noreply.github.com>
2022-11-25 04:01:00 +0000

    Modals

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component, useState } from 'react';
 import ListCast from './components/ListCast';
+import Modals from './components/Modals';
 
 function App() {
   const name = 'StarGazers'
@@ -13,20 +14,7 @@ function App() {
           paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
         <ListCast onChoice={(info) => { setMemberInfor(info) }} />
         {memberInfo &&
-          <article>
-            <hgroup>
-              <div style={{
-                display: "flex",
-                gap: '1rem',
-              }}>
-                <img style={{ width: '200px' }} src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name} />
-                <hgroup>
-                  <h1>{memberInfo.name}</h1>
-                  <p>{memberInfo.bio}</p>
-                </hgroup>
-              </div>
-            </hgroup>
-          </article>
+          <Modals member={memberInfo} handleClose={() => setMemberInfor(null)} />
         }
       </hgroup>
     </div>
